7ad68179d1328b0794f7358e2527b4f5a1ea9384
Purvish Panchal <purvishpanchal147@gmail.com>
2026-06-29 18:31:44 +0530

    Loop-Swtich-variable-Java

---
+++ JAVA/Loops.java
@@ -0,0 +1,33 @@
+//ForLoop
+// public class ForLoop {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+// While Loop
+// public class WhileLoop {
+//     public static void main(String[] args) {
+//         int i = 1;
+
+//         while (i <= 5) {
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+
+// Do-While Loop
+// public class DoWhileLoop {
+//     public static void main(String[] args) {
+//         int i = 1;
+
+//         do {
+//             System.out.println(i);
+//             i++;
+//         } while (i <= 5);
+//     }
+// }
