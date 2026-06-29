c26f89f7b78a405b662961b100690c205ef9d8be
Purvish Panchal <purvishpanchal147@gmail.com>
2026-06-29 18:24:04 +0530

    Array-Java

---
+++ JAVA/Array.java
@@ -0,0 +1,22 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
+
+//Enhanced For Loop
+// public class EnhancedFor {
+//     public static void main(String[] args) {
+//         int[] arr = {5, 10, 15};
+
+//         for (int x : arr) {
+//             System.out.println(x);
+//         }
+//     }
+// }
