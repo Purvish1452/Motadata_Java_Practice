d8ae912cd4bf2c68a54b472d4d3d41e75c3efffe
Purvish Panchal <purvishpanchal147@gmail.com>
2026-06-30 18:42:44 +0530

    Exception--Java

---
+++ JAVA/OOps/Exception.java
@@ -0,0 +1,12 @@
+public class Exception {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        System.out.println("Program End");
+    }
+}
