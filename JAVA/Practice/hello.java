8619b6a444372896bdd3b4d7b4fdb76a55a34443
Purvish Panchal <purvishpanchal147@gmail.com>
2026-07-02 00:11:03 +0530

    Practice--Java

---
+++ JAVA/Practice/hello.java
@@ -0,0 +1,99 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("jay mahakaal");
+
+
+        //Data types
+        // System.out.println(3+5);
+        // int a=5;
+        // System.out.println(a);
+        // float b=5.5f;
+        // System.out.println(b);
+        // boolean c=true;
+        // System.out.println(c);
+        // double g=28.2;
+        // System.out.println(g);
+
+
+
+
+
+        //literals
+        // int l=0X101;
+        // System.out.println(l);
+        // int p=10_10_1000;
+        // System.out.println(p);
+        // double ll=12e8;
+        // System.out.println(ll);
+
+
+
+
+        // Type casting 
+        // byte x=127;
+        // int aa=257;
+        // x=(byte)aa;  //casting it modulo 256 because byte can only store values from -128 to 127
+        // System.out.println(x);
+
+
+
+        // Operators
+        // int a=10;
+        // int b=a++; //post increment, b gets the value of a before incrementing, so b=10 and then a becomes 11
+        // int c=++a; //pre increment, a is incremented first to 12 and then c gets the value of a, so c=12
+        // System.out.println(b); //11
+        // System.out.println(c); //12
+        // System.out.println(a); //12
+    
+        //Ternary operator
+        // int a=4;
+        // int result= a>5?15:20;
+        // System.out.println(result);
+
+
+        //Switch case
+        // int day=7;
+        // switch(day)
+        // {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     default://if none of the cases match, this block will be executed
+        //         System.out.println("Invalid day");
+        // }
+
+
+
+        while(true)
+        {
+            System.out.println("Hello");
+        }
+
+        
+
+
+
+
+        
+
+    }
+}
+
+
+
+
+
+
+        
+
+    
